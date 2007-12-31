ef6f4398ef2d1ce635f8e70be34dc8a275802c0a
giles_bowkizzo <giles_bowkizzo@624654a9-0480-469b-a93b-0723543e320f>
2007-12-31 23:50:18 +0000

    New version works across platforms and takes optional string arg.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/utilitybelt@32 624654a9-0480-469b-a93b-0723543e320f

--- lib/pastie.rb
+++ lib/pastie.rb
@@ -1,15 +1,21 @@
-# automate creating pasties
+# # automate creating pasties
+%w{platform net/http mac_clipboard}.each {|lib| require lib}
 class Object
-  if :macosx == Platform::IMPL
-    def pastie
-      pastie_url = Net::HTTP.post_form(URI.parse("http://pastie.caboo.se/pastes/create"),
-                                       {"paste_parser" => "ruby",
-                                        "paste[authorization]" => "burger",
-                                        "paste[body]" => MacClipboard.read}).body.match(/href="([^\"]+)"/)[1]
+  def pastie(stuff_to_paste = nil)
+    stuff_to_paste ||= MacClipboard.read if :macosx == Platform::IMPL
+    # return nil unless stuff_to_paste
+
+    pastie_url = Net::HTTP.post_form(URI.parse("http://pastie.caboo.se/pastes/create"),
+                                     {"paste_parser" => "ruby",
+                                      "paste[authorization]" => "burger",
+                                      "paste[body]" => stuff_to_paste}).body.match(/href="([^\"]+)"/)[1]
+
+    if :macosx == Platform::IMPL
       MacClipboard.write(pastie_url)
-      system("open #{pastie_url}")
-      pastie_url
+      Kernel.system("open #{pastie_url}")
     end
-    alias :pst :pastie
+
+    return pastie_url
   end
+  alias :pst :pastie
 end
