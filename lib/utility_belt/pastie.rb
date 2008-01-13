ec39edda6c4da88982edea94b898f475f746f13a
cypher <cypher@624654a9-0480-469b-a93b-0723543e320f>
2008-01-13 19:07:49 +0000

    Put Pastie into its own module
    git-svn-id: svn+ssh://rubyforge.org/var/svn/utilitybelt@51 624654a9-0480-469b-a93b-0723543e320f

--- lib/utility_belt/pastie.rb
+++ lib/utility_belt/pastie.rb
@@ -1,21 +1,27 @@
 # automate creating pasties
-%w{platform net/http utility_belt/clipboard}.each {|lib| require lib}
-class Object
-  def pastie(stuff_to_paste = nil)
-    stuff_to_paste ||= Clipboard.read if Clipboard.available?
-    # return nil unless stuff_to_paste
+%w{platform net/http utility_belt}.each {|lib| require lib}
+module UtilityBelt
+  module Pastie
+    def pastie(stuff_to_paste = nil)
+      stuff_to_paste ||= Clipboard.read if Clipboard.available?
+      # return nil unless stuff_to_paste
 
-    pastie_url = Net::HTTP.post_form(URI.parse("http://pastie.caboo.se/pastes/create"),
-                                     {"paste_parser" => "ruby",
-                                      "paste[authorization]" => "burger",
-                                      "paste[body]" => stuff_to_paste}).body.match(/href="([^\"]+)"/)[1]
+      pastie_url = Net::HTTP.post_form(URI.parse("http://pastie.caboo.se/pastes/create"),
+                                       {"paste_parser" => "ruby",
+                                        "paste[authorization]" => "burger",
+                                        "paste[body]" => stuff_to_paste}).body.match(/href="([^\"]+)"/)[1]
 
-    if :macosx == Platform::IMPL
-      MacClipboard.write(pastie_url)
-      Kernel.system("open #{pastie_url}")
+      if :macosx == Platform::IMPL
+        MacClipboard.write(pastie_url)
+        Kernel.system("open #{pastie_url}")
+      end
+
+      return pastie_url
     end
-
-    return pastie_url
+    alias :pst :pastie
   end
-  alias :pst :pastie
 end
+
+class Object
+  include UtilityBelt::Pastie
+end if Object.const_defined? :IRB
