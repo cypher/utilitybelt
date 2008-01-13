ac592c7dffcdfda59a0506df39ce77ef26f73bed
cypher <cypher@624654a9-0480-469b-a93b-0723543e320f>
2008-01-13 15:35:07 +0000

    Fixed google gadget and binary to use the equipper and to open URLs on Windows as well.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/utilitybelt@42 624654a9-0480-469b-a93b-0723543e320f

--- lib/utility_belt/google.rb
+++ lib/utility_belt/google.rb
@@ -1,18 +1,31 @@
 #!/usr/bin/env ruby
-%w{rubygems platform cgi utility_belt/mac_clipboard}.each {|library| require library}
+%w{rubygems platform cgi}.each {|library| require library}
 
-class Object
-  if :macosx == Platform::IMPL
+UtilityBelt.equip(:mac_clipboard)
+
+module UtilityBelt
+  module Google
     def google(search_term = nil)
-      search_term ||= MacClipboard.read
+      search_term ||= MacClipboard.read if :macosx == Platform::IMPL
       if search_term.empty?
         puts "Usage: google search_term_without_spaces           (Unix command line only)"
         puts "       google 'search term with spaces'            (Unix or IRB)"
         puts "       google                                      (Unix or IRB)"
         puts "              (if invoking without args, must have text in clipboard)"
       else
-        system("open http://google.com/search?q=#{CGI.escape(search_term)}")
+        url = "http://google.com/search?q=#{CGI.escape(search_term)}"
+        case Platform::IMPL
+        when :macosx
+          system("open #{url}")
+        when :windows
+          system("start #{url}")
+        #when :linux
+        else
+          puts "Sorry, don't know how to open an URL from the command line on your platform"
+        end
       end
     end
   end
 end
+
+Object.extend(UtilityBelt::Google)
