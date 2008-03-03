47bdc0ed7a6a764f56d0564839ccce3d5b7e3c16
Markus Prinz <markus.prinz@qsig.org>
2008-03-03 21:28:55 +0100

    Make Hashmath Substraction non-destructive (Patch by Jimmy Whiteman)

--- lib/utility_belt/hash_math.rb
+++ lib/utility_belt/hash_math.rb
@@ -1,13 +1,10 @@
 class Hash
   alias :+ :merge
-  def -(thing_to_be_deleted)
-    if thing_to_be_deleted.is_a? Hash
-      thing_to_be_deleted.each do |key, value|
-        self.delete(key) if self[key] == value
-      end
-    elsif self.keys.include? thing_to_be_deleted
-      self.delete(thing_to_be_deleted)
+  def -(other)
+    difference = if other.is_a? Hash
+      reject { |k,v| other[k] == v }
+    else
+      reject { |k,v| k == other }
     end
-    self
   end
 end
--- spec/hash_math_spec.rb
+++ spec/hash_math_spec.rb
@@ -1,7 +1,13 @@
 require File.join(File.dirname(__FILE__), "spec_helper")
 require "lib/utility_belt/hash_math"
 describe "Hash math" do
-
+  
+  before do
+    @a = { :a => 1 }
+    @b = { :b => 2 }
+    @c = { :a => 1, :b => 2}
+  end
+  
   it "should add hashes" do
     ({:a => :b} + {:c => :d}).should == {:a => :b, :c => :d}
   end
@@ -12,6 +18,15 @@ describe "Hash math" do
 
   it "should subtract key/value pairs by key" do
     ({:a => :b, :c => :d} - :c).should == {:a => :b}
+  end
+  
+  it "should return a new object instead of mutating the operands" do
+    (@c - @a).should_not === @c
+  end
+  
+  it "should not alter the state of the operands" do
+    (@c - @b).should == @a # but...
+    @c.should == { :a => 1, :b => 2 }
   end
 
 end
